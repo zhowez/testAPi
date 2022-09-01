665cc8bbdd9de98f137d82ee969f2a16f3d38752
Zach Howes <zhowes14@icloud.com>
2022-08-31 22:46:46 -0400

    added bpdy parser so I could test json

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
 
-app.get("/home", (_req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/api/home", (_req, res) => {
   res.status(200);
   console.log("The API was HIT!!!");
   res.send("The proxy worked!!!");
+});
+
+app.post("/api/post", (req, res) => {
+  console.log(req.body);
+  if (req.body.work == "please") {
+    res.status(200);
+    res.send("it worked");
+  } else {
+    res.status(300);
+    res.send("nope");
+  }
 });
 
 app.listen(PORT, (error) => {
